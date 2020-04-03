090f180b050767d868045888697fc601e6e45f61
Jay Harris <jay.harris@outlook.co.nz>
2020-04-03 16:05:17 +1100

    Gets server rendering working with MUI

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,19 @@
 import "react-big-calendar/lib/css/react-big-calendar.css"
+import { useEffect } from "react";
+import { ThemeProvider, CssBaseline } from "@material-ui/core";
+import theme from "../utils/theme";
 
 export default ({ Component, pageProps }) => {
-    return <Component {...pageProps}/>
+    useEffect(() => {
+        // Remove the server-side injected CSS.
+        const jssStyles = document.querySelector('#jss-server-side');
+        if (jssStyles) {
+          jssStyles.parentElement.removeChild(jssStyles);
+        }
+      }, []);
+
+    return <ThemeProvider theme={theme}>
+        <CssBaseline/>
+        <Component {...pageProps}/>
+    </ThemeProvider>;
 }
--- pages/admin/overview.tsx
+++ pages/admin/overview.tsx
@@ -4,11 +4,16 @@ import moment from 'moment';
 import { makeStyles } from "@material-ui/core";
 
 const localizer = momentLocalizer(moment);
+const views = {
+    month: true,
+    week: true
+};
+
 const useStyles = makeStyles(theme => ({
     calendar: {
         height: '500px'
     }
-}))
+}));
 export default () => {
     const month = 7;
     const year = new Date().getFullYear();
@@ -16,7 +21,8 @@ export default () => {
     const classes = useStyles();
     return <Layout title="Overview">
         <Calendar
-            elementProps={{ className: classes.calendar }}
+            views={views}
+            className={classes.calendar}
             events={[{ start: new Date(), end: new Date() }]}
             localizer={localizer}/>
     </Layout>
