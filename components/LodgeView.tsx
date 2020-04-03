7f66f380e6ac433a6e5ed08fe19788afe13b1aa8
Jay Harris <jay.harris@outlook.co.nz>
2020-04-03 15:16:00 +1100

    Hacky lodge view

---
+++ components/LodgeView.tsx
@@ -0,0 +1,10 @@
+import { Lodge } from "../model/lodge";
+import lodges from "../pages/lodges";
+
+export default ({ lodge }: { lodge: Lodge }) => {
+    return <div>
+        <h2>{lodge.name}</h2>
+        <div>Occupancy: {lodge.occupancy}</div>
+        <div>Prefer Students: {lodge.preferStudents ? "yes" : "no"}</div>
+    </div>;
+}
--- pages/lodges/index.tsx
+++ pages/lodges/index.tsx
@@ -1,10 +1,14 @@
 import Layout from "../../components/Layout"
 import useData from "../../services/useData"
 import { Lodge } from "../../model/lodge"
+import { CircularProgress } from "@material-ui/core";
+import LodgeView from "../../components/LodgeView";
 
 export default () => {
     const data = useData<Lodge[]>('/api/lodges');
     return <Layout title="Lodges">
-        {JSON.stringify(data)}
+        {!data
+            ? <CircularProgress />
+            : data.map(lodge => <LodgeView lodge={lodge} key={lodge.id} />)}
     </Layout>
 }
