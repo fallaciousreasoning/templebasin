10819bd15efa5bae79a0df6d7581a17c1f76a4d1
Jay Harris <jay.harris@outlook.co.nz>
2020-04-03 17:06:48 +1100

    Begins work on sidebar

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,6 +1,9 @@
 import Head from "next/head";
-import { AppBar, Toolbar, IconButton, makeStyles, Typography, Button, Paper } from '@material-ui/core';
+import { AppBar, Toolbar, IconButton, makeStyles, Typography, Button, Paper, Drawer, ListItem, List, ListItemText } from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
+import { useState, useCallback } from "react";
+import { useRouter } from "next/dist/client/router";
+import Link from 'next/link';
 
 interface Props {
     title?: string;
@@ -22,10 +25,19 @@ const useStyles = makeStyles((theme) => ({
     title: {
         flexGrow: 1,
     },
+    drawer: {
+        width: 250
+    }
 }));
 
 export default (props: Props) => {
     const classes = useStyles();
+    const [drawerOpen, setDrawerOpen] = useState(false);
+
+    const toggleDrawer = useCallback(() => {
+        setDrawerOpen(!drawerOpen);
+    }, [drawerOpen]);
+    
     return <div className="layout">
         <Head>
             <title>{props.title}</title>
@@ -37,7 +49,7 @@ export default (props: Props) => {
         `}</style>
         <AppBar position="static">
             <Toolbar>
-                <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu" onClick={toggleDrawer}>
                     <MenuIcon />
                 </IconButton>
                 <Typography variant="h6" className={classes.title}>
@@ -45,6 +57,24 @@ export default (props: Props) => {
                 </Typography>
             </Toolbar>
         </AppBar>
+        <Drawer anchor="left" open={drawerOpen} onClose={toggleDrawer}>
+            <div className={classes.drawer}>
+                <List>
+                    <ListItem button>
+                        <ListItemText primary={<Link href="/admin/overview">Overview</Link>}/>
+                    </ListItem>
+                    <ListItem button>
+                        <ListItemText primary={<Link href="/bookings">Bookings</Link>}/>
+                    </ListItem>
+                    <ListItem button>
+                        <ListItemText primary={<Link href="/bookings/book">New Booking</Link>}/>
+                    </ListItem>
+                    <ListItem button>
+                        <ListItemText primary={<Link href="/admin/lodges">Lodges</Link>}/>
+                    </ListItem>
+                </List>
+            </div>
+        </Drawer>
         <Paper className={classes.content}>
             {props.children}
         </Paper>
