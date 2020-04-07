a76fece31bc247fc3cf7877562fecc2b76720bd8
Jay Harris <jay.harris@outlook.co.nz>
2020-04-07 11:54:05 +1000

    Don't set stale data

--- services/useData.ts
+++ services/useData.ts
@@ -4,13 +4,24 @@ export default <T>(url: string, defaultValue: T = undefined) => {
     const [data, setData] = useState<T>(defaultValue);
 
     useEffect(() => {
+        let isValid = true;
+
         setData(defaultValue);
         if (!url)
             return;
-            
+
         fetch(url)
             .then(url => url.json())
-            .then(setData);
+            .then(data => {
+                if (!isValid)
+                    return;
+
+                setData(data);
+            });
+
+        return () => {
+            isValid = false;
+        }
     }, [url]);
 
     return data;
