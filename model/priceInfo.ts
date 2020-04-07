8da5ae0ee8bf05d01eebd49712cd843b93c88b5f
Jay Harris <jay.harris@outlook.co.nz>
2020-04-07 17:41:44 +1000

    Adds pricing mechanism

--- model/priceInfo.ts
+++ model/priceInfo.ts
@@ -1,5 +1,30 @@
 import { BookingInfo } from "./bookingInfo";
 import { Guest, AccomodationCategory } from "./guest";
+import { getPrices } from "../services/pricing";
+
+export interface Prices {
+    liftPasses: PriceInfo;
+
+    lunchOnly: PriceInfo;
+    weekender: PriceInfo;
+
+    dbb: PriceInfo;
+    
+    // Self catered.
+    accomodationOnly: PriceInfo
+    
+    // Packages: 
+    // index is [numNights - 1];
+    packages: [
+        PriceInfo,
+        PriceInfo,
+        PriceInfo,
+        PriceInfo,
+        PriceInfo,
+        PriceInfo,
+        PriceInfo
+    ];
+}
 
 export interface PriceInfoBase {
     default?: number;
@@ -8,13 +33,15 @@ export interface PriceInfoBase {
 }
 
 export interface PriceInfo extends PriceInfoBase {
+    isPerDay?: boolean;
+
     adult?: PriceInfoBase | number;
     student?: PriceInfoBase | number;
     under18?: PriceInfoBase | number;
     under13?: PriceInfoBase | number;
 }
 
-const accomodationCategory: { [key: string]: keyof PriceInfo} = {
+const accomodationCategory: { [key: string]: 'adult' | 'student' | 'under18' | 'under13' & keyof PriceInfo } = {
     "Adult": "adult",
     "Student": "student",
     "Under 18": "under18",
@@ -55,4 +82,34 @@ export const determinePrice = (guest: Guest, price: PriceInfo) => {
         throw new Error("Unable to determine price!");
 
     return rate;
+}
+
+const priceForStay = (guest: Guest, booking: BookingInfo, prices: Prices) => {
+    const { selfCatered, duration, includeLiftTickets } = booking;
+    let total = 0;
+    const addPriceForDays = (days: number, scheme: PriceInfo) =>
+        total += (scheme.isPerDay ? days : 1) * determinePrice(guest, scheme);
+
+    if (selfCatered) {
+        addPriceForDays(duration, prices.accomodationOnly);
+    } else if (!includeLiftTickets) {
+        addPriceForDays(duration, prices.dbb);
+    } else {
+        const packageIndex = duration - 1;
+        if (packageIndex >= prices.packages.length)
+            throw new Error(`No package covering ${duration} days!`);
+        addPriceForDays(duration, prices.packages[packageIndex]);
+    }
+    
+    return total;
+}
+
+export const calculatePrice = async (booking: BookingInfo) => {
+    const prices = await getPrices();
+    let total = 0;
+
+    for (const guest of booking.guests) {
+        const guestCost = priceForStay(guest, booking, prices);
+        total += guestCost;
+    }
 }
