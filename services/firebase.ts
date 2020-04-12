6dd050d6386393d85b7c56f541049bb5dbe53673
Jay Harris <jay.harris@outlook.co.nz>
2020-04-12 17:12:18 +1000

    Use firebase admin config from FIREBASE_CONFIG

--- services/firebase.ts
+++ services/firebase.ts
@@ -1,11 +1,12 @@
 import * as admin from 'firebase-admin';
 
 const config = require('../firebase.config.json');
+const adminConfig = JSON.parse(process.env.FIREBASE_CONFIG);
 
 if (!admin.apps.length) {
     admin.initializeApp({
         ...config,
-        credential: admin.credential.applicationDefault(),
+        credential: admin.credential.cert(adminConfig)
     });
 }
 
