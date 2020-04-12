e91491f9a734abc4f889276be337c6402edf3ba4
Jay Harris <jay.harris@outlook.co.nz>
2020-04-12 16:40:10 +1000

    Begins using admin sdk

--- services/firebase.ts
+++ services/firebase.ts
@@ -1,8 +1,12 @@
-import * as admin from 'firebase';
+import * as admin from 'firebase-admin';
 
 const config = require('../firebase.config.json');
 
-if (!admin.apps.length)
-    admin.initializeApp(config);
+if (!admin.apps.length) {
+    admin.initializeApp({
+        ...config,
+        credential: admin.credential.applicationDefault(),
+    });
+}
 
 export const database = admin.database();
